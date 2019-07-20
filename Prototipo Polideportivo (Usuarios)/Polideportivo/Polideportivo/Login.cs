c4d90a924fbf664df34da208522f8c5bb367c7fe
EduardoColon <eduardo.colon.15@gmail.com>
2019-07-19 17:40:07 -0700

    Commit inicial
    Commit inicial Prototipo [usuarios]

---
+++ Prototipo Polideportivo (Usuarios)/Polideportivo/Polideportivo/Login.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Autor: Allan letona
+
+namespace Polideportivo
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SeleccionarEquipo seleccionar = new SeleccionarEquipo();
+            seleccionar.Show();
+            Hide();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
