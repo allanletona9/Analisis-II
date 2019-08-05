ba3b25ecf1e95f2334b4e6f9296abd40abef21de
EduardoColon <eduardo.colon.15@gmail.com>
2019-08-05 08:31:52 -0600

    Polideportivo usuario

---
+++ PolideportivoUsuarios/Polideportivo Usuarios/Polideportivo/Menu.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Polideportivo
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.miEquipoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.misJugadoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.partidosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calendarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seleccionarEquipoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estadisticasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tablaDePosicionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mejoresJugadoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sancionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.direccionDeConceptosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.preguntasFrecuentesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.miCuentaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cambiarClaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarSesionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miEquipoToolStripMenuItem,
+            this.estadisticasToolStripMenuItem,
+            this.ayudaToolStripMenuItem,
+            this.reportesToolStripMenuItem,
+            this.miCuentaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(984, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // miEquipoToolStripMenuItem
+            // 
+            this.miEquipoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.misJugadoresToolStripMenuItem,
+            this.partidosToolStripMenuItem,
+            this.calendarioToolStripMenuItem,
+            this.seleccionarEquipoToolStripMenuItem});
+            this.miEquipoToolStripMenuItem.Name = "miEquipoToolStripMenuItem";
+            this.miEquipoToolStripMenuItem.Size = new System.Drawing.Size(73, 22);
+            this.miEquipoToolStripMenuItem.Text = "Mi Equipo";
+            // 
+            // misJugadoresToolStripMenuItem
+            // 
+            this.misJugadoresToolStripMenuItem.Name = "misJugadoresToolStripMenuItem";
+            this.misJugadoresToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.misJugadoresToolStripMenuItem.Text = "Mis jugadores";
+            this.misJugadoresToolStripMenuItem.Click += new System.EventHandler(this.misJugadoresToolStripMenuItem_Click);
+            // 
+            // partidosToolStripMenuItem
+            // 
+            this.partidosToolStripMenuItem.Name = "partidosToolStripMenuItem";
+            this.partidosToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.partidosToolStripMenuItem.Text = "Consultar jornadas";
+            this.partidosToolStripMenuItem.Click += new System.EventHandler(this.partidosToolStripMenuItem_Click);
+            // 
+            // calendarioToolStripMenuItem
+            // 
+            this.calendarioToolStripMenuItem.Name = "calendarioToolStripMenuItem";
+            this.calendarioToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.calendarioToolStripMenuItem.Text = "Calendario";
+            this.calendarioToolStripMenuItem.Click += new System.EventHandler(this.calendarioToolStripMenuItem_Click);
+            // 
+            // seleccionarEquipoToolStripMenuItem
+            // 
+            this.seleccionarEquipoToolStripMenuItem.Name = "seleccionarEquipoToolStripMenuItem";
+            this.seleccionarEquipoToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.seleccionarEquipoToolStripMenuItem.Text = "Cambiar equipo";
+            this.seleccionarEquipoToolStripMenuItem.Click += new System.EventHandler(this.seleccionarEquipoToolStripMenuItem_Click);
+            // 
+            // estadisticasToolStripMenuItem
+            // 
+            this.estadisticasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tablaDePosicionesToolStripMenuItem,
+            this.mejoresJugadoresToolStripMenuItem,
+            this.sancionesToolStripMenuItem});
+            this.estadisticasToolStripMenuItem.Name = "estadisticasToolStripMenuItem";
+            this.estadisticasToolStripMenuItem.Size = new System.Drawing.Size(79, 22);
+            this.estadisticasToolStripMenuItem.Text = "Estadisticas";
+            // 
+            // tablaDePosicionesToolStripMenuItem
+            // 
+            this.tablaDePosicionesToolStripMenuItem.Name = "tablaDePosicionesToolStripMenuItem";
+            this.tablaDePosicionesToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.tablaDePosicionesToolStripMenuItem.Text = "Tabla de posiciones";
+            this.tablaDePosicionesToolStripMenuItem.Click += new System.EventHandler(this.tablaDePosicionesToolStripMenuItem_Click);
+            // 
+            // mejoresJugadoresToolStripMenuItem
+            // 
+            this.mejoresJugadoresToolStripMenuItem.Name = "mejoresJugadoresToolStripMenuItem";
+            this.mejoresJugadoresToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.mejoresJugadoresToolStripMenuItem.Text = "Mejores jugadores";
+            this.mejoresJugadoresToolStripMenuItem.Click += new System.EventHandler(this.mejoresJugadoresToolStripMenuItem_Click);
+            // 
+            // sancionesToolStripMenuItem
+            // 
+            this.sancionesToolStripMenuItem.Name = "sancionesToolStripMenuItem";
+            this.sancionesToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.sancionesToolStripMenuItem.Text = "Sanciones";
+            this.sancionesToolStripMenuItem.Click += new System.EventHandler(this.sancionesToolStripMenuItem_Click);
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.direccionDeConceptosToolStripMenuItem,
+            this.preguntasFrecuentesToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 22);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // direccionDeConceptosToolStripMenuItem
+            // 
+            this.direccionDeConceptosToolStripMenuItem.Name = "direccionDeConceptosToolStripMenuItem";
+            this.direccionDeConceptosToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.direccionDeConceptosToolStripMenuItem.Text = "Diccionario de conceptos";
+            this.direccionDeConceptosToolStripMenuItem.Click += new System.EventHandler(this.direccionDeConceptosToolStripMenuItem_Click);
+            // 
+            // preguntasFrecuentesToolStripMenuItem
+            // 
+            this.preguntasFrecuentesToolStripMenuItem.Name = "preguntasFrecuentesToolStripMenuItem";
+            this.preguntasFrecuentesToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.preguntasFrecuentesToolStripMenuItem.Text = "Preguntas frecuentes";
+            this.preguntasFrecuentesToolStripMenuItem.Click += new System.EventHandler(this.preguntasFrecuentesToolStripMenuItem_Click);
+            // 
+            // reportesToolStripMenuItem
+            // 
+            this.reportesToolStripMenuItem.Name = "reportesToolStripMenuItem";
+            this.reportesToolStripMenuItem.Size = new System.Drawing.Size(65, 22);
+            this.reportesToolStripMenuItem.Text = "Reportes";
+            // 
+            // miCuentaToolStripMenuItem
+            // 
+            this.miCuentaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cambiarClaveToolStripMenuItem,
+            this.cerrarSesionToolStripMenuItem});
+            this.miCuentaToolStripMenuItem.Name = "miCuentaToolStripMenuItem";
+            this.miCuentaToolStripMenuItem.Size = new System.Drawing.Size(72, 22);
+            this.miCuentaToolStripMenuItem.Text = "Mi cuenta";
+            // 
+            // cambiarClaveToolStripMenuItem
+            // 
+            this.cambiarClaveToolStripMenuItem.Name = "cambiarClaveToolStripMenuItem";
+            this.cambiarClaveToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.cambiarClaveToolStripMenuItem.Text = "Cambiar clave";
+            this.cambiarClaveToolStripMenuItem.Click += new System.EventHandler(this.cambiarClaveToolStripMenuItem_Click);
+            // 
+            // cerrarSesionToolStripMenuItem
+            // 
+            this.cerrarSesionToolStripMenuItem.Name = "cerrarSesionToolStripMenuItem";
+            this.cerrarSesionToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.cerrarSesionToolStripMenuItem.Text = "Cerrar sesion";
+            this.cerrarSesionToolStripMenuItem.Click += new System.EventHandler(this.cerrarSesionToolStripMenuItem_Click);
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.BackColor = System.Drawing.Color.DarkGray;
+            this.lblUsuario.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(873, 5);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(99, 19);
+            this.lblUsuario.TabIndex = 84;
+            this.lblUsuario.Text = "lblUsuario";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumBlue;
+            this.ClientSize = new System.Drawing.Size(984, 487);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.menuStrip1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem miEquipoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem misJugadoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem partidosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem estadisticasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tablaDePosicionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mejoresJugadoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sancionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem direccionDeConceptosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem preguntasFrecuentesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem miCuentaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cambiarClaveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarSesionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calendarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seleccionarEquipoToolStripMenuItem;
+        private System.Windows.Forms.Label lblUsuario;
+    }
+}
---
+++ PolideportivoUsuarios/Polideportivo Usuarios/Polideportivo/SeleccionarEquipo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Autor: Eduardo Colon
+
+namespace Polideportivo
+{
+    public partial class SeleccionarEquipo : Form
+    {
+        String sIdEquipo, sOrigen,usuario;
+
+
+        public SeleccionarEquipo(String origen, String equipo, string text)
+        {
+            this.usuario = text;
+            InitializeComponent();
+            this.sIdEquipo = sIdEquipo;
+            this.sOrigen = origen;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Menu menu = new Menu(Cbo_seleccionar_equipo.Text,usuario);
+            menu.Show();
+            Hide();
+        }
+
+        private void SeleccionarEquipo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_regresar_Click(object sender, EventArgs e)
+        {
+
+            if(sOrigen == "Menu")
+            {
+                Menu menu = new Menu(sIdEquipo,usuario);
+                menu.Show();
+                Hide();
+            }
+            else
+            {
+                Login login = new Login();
+                login.Show();
+                Hide();
+            }
+
+        }
+    }
+}
